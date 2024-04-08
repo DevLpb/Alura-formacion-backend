0e8d481ca4b3a531d3cf85f3ff9bde5f901a2b2f
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-04-08 11:15:20 -0300

    Uso de condicionales

---
+++ Primera-app-Java/src/Decisiones.java
@@ -0,0 +1,14 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento >= 2022) {
+            System.out.println("Peliculas más populares");
+        } else {
+            System.out.println("Peliculas que aùn vale la pena ver");
+        }
+    }
+}
