436c88cc9745c838d4f51f6e71037c9cb9f0e775
Leonel <leonelpiedrabuena04@gmail.com>
2024-04-09 23:51:43 -0300

    Clases 2
    Encapsulación, modificadores de acceso, lectura y modificación de atributos, paquetes.

--- Alura-ScreenMatch/ScreenMatch/src/Principal.java
+++ Alura-ScreenMatch/ScreenMatch/src/Principal.java
@@ -1,28 +1,34 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
 
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Matrix";
-        miPelicula.fechaDeLanzamiento = 1999;
-        miPelicula.duracionEnMinutos = 136;
+        miPelicula.setNombre("Matrix");
+        miPelicula.setFechaDeLanzamiento(1999);
+        miPelicula.setDuracionEnMinutos(136);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
+
         miPelicula.evalua(10);
         miPelicula.evalua(9);
         miPelicula.evalua(10);
 
-        System.out.println(miPelicula.sumaDeEvaluaciones);
-        System.out.println(miPelicula.totalDeEvaluaciones);
+        System.out.println(miPelicula.getTotalDeEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Ghost in the Shell";
-        otraPelicula.fechaDeLanzamiento = 1995;
-        otraPelicula.duracionEnMinutos = 83;
 
-        otraPelicula.muestraFichaTecnica();
-        otraPelicula.evalua(10);
-        miPelicula.evalua(9);
-        miPelicula.evalua(9);
+
+
+
+
+//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
+//        otraPelicula.nombre = "Ghost in the Shell";
+//        otraPelicula.fechaDeLanzamiento = 1995;
+//        otraPelicula.duracionEnMinutos = 83;
+//
+//        otraPelicula.muestraFichaTecnica();
+//        otraPelicula.evalua(10);
     }
 }
---
+++ Alura-ScreenMatch/ScreenMatch/src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,62 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Pelicula {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeEvaluaciones;
+    private int totalDeEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeEvaluaciones() {
+        return totalDeEvaluaciones;
+    }
+
+    public void muestraFichaTecnica() {
+        System.out.println("El nombre de la película es: " + getNombre());
+        System.out.println("Su fecha de lanzamiento es: " + getFechaDeLanzamiento());
+        System.out.println("Su duración en minutos es: " + getDuracionEnMinutos());
+        System.out.println(isIncluidoEnElPlan());
+    }
+
+    public void evalua(double nota) {
+        sumaDeEvaluaciones += nota;
+        totalDeEvaluaciones++;
+    }
+
+    public double calculaMedia() {
+        return sumaDeEvaluaciones / totalDeEvaluaciones;
+    }
+}
