305cbfea72b4b805a69eb040883e46f2f4bb8436
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-04-16 11:50:05 -0300

    Primer uso de API
    Consultas e integración de API usando HttpClient, HttpRequest y HttpResponse.

---
+++ Alura-ScreenMatch/ScreenMatch/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,29 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el nombre de la película: ");
+        var busqueda = lectura.nextLine();
+
+        String clave = "28749dc6";
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey="+clave;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
