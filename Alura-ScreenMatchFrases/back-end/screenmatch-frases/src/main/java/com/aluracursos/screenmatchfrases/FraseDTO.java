33b26df487de2a1004b745755f5943c95af148b6
Leonel-exo <leonelpiedrabuena04@gmail.com>
2024-05-19 22:27:31 -0300

    Desafio Java y Spring, frases

---
+++ Alura-ScreenMatchFrases/back-end/screenmatch-frases/src/main/java/com/aluracursos/screenmatchfrases/FraseDTO.java
@@ -0,0 +1,12 @@
+package com.aluracursos.screenmatchfrases;
+
+public record FraseDTO(
+         String titulo,
+
+         String frase,
+
+         String personaje,
+
+         String poster
+) {
+}
